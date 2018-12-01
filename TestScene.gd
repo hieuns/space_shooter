235103bcc13c7ec9f57f2007013795e9d580c7de
Nguyen Sy Hieu <hieuns.a2@gmail.com>
2018-12-01 12:21:12 +0700

    Remove position node, use zindex in editor

--- TestScene.gd
+++ TestScene.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
-func _ready():
-  # Called when the node is added to the scene for the first time.
-  # Initialization here
-  $Player.position = $PlayerStartingPosition.position
-  $Player.setZIndex(1)
-
 func _on_Player_shoot(Bullet, _position, _direction):
   var bullet_instance = Bullet.instance()
   add_child(bullet_instance)
-  bullet_instance.start(_position, _direction, 0)
+  bullet_instance.start(_position, _direction)
